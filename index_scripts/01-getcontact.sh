3dc7e7c53734239a9b4630ac3bf36583a18c668d
Carlos Moreira - Macbook <cmoreira@misfirm.com>
2025-03-18 19:46:30 -0400

    Cleanup

--- index_scripts/01-getcontact.sh
+++ index_scripts/01-getcontact.sh
@@ -1,5 +1,5 @@
 . ./.env
 
-curl -k --request GET \
---location "https://$FQDN_IP/api/cluster?fields=contact" \
---header "Authorization: Basic $BASIC_AUTH"
+curl -k -X GET "https://$FQDN_IP/api/cluster?fields=contact" \
+-H "accept: application/json" \
+-H "Authorization: Basic $BASIC_AUTH"
--- index_scripts/02-getsvms.sh
+++ index_scripts/02-getsvms.sh
@@ -1,5 +1,5 @@
 . ./.env
 
-curl -k -X GET "https://${FQDN_IP}/api/svm/svms?return_records=true&return_timeout=15" \
+curl -k -X GET "https://$FQDN_IP/api/svm/svms?return_records=true&return_timeout=15" \
 -H "accept: application/json" \
--H "authorization: Basic ${BASIC_AUTH}"
+-H "Authorization: Basic $BASIC_AUTH"
